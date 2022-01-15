a54a206d3bdbb0cb71087a2fd1a5a715dcb918f0
atsushi0919 <hatake0919@yahoo.co.jp>
2022-01-15 15:25:08 +0900

    01-04_single_pop C# AC

---
+++ 01-04_single_pop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_04_single_pop
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 入力
+            int n = int.Parse(Console.ReadLine());
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                queue.Enqueue(value);
+            }
+
+            // Dequeue
+            queue.Dequeue();
+
+            // 出力
+            Console.WriteLine(String.Join("\n", queue));
+        }
+    }
+}
