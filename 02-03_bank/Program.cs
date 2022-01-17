a35ec6405fde85f4f8566948e6eca7f69cc3f1a3
atsushi0919 <hatake0919@yahoo.co.jp>
2022-01-17 17:09:14 +0900

    02-03_bank C# AC

--- 02-03_bank/Program.cs
+++ 02-03_bank/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02_03_bank
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // 入力, 初期化
+            string[] input = Console.ReadLine().Split();
+            // n: 会社数, k: 取引数
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+            string[] companies = new string[n];
+            // 会社情報入力, 口座情報初期化
+            var bankAccountTable =
+                new Dictionary<string, Tuple<string, int>>();
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split();
+                string companyName = input[0];
+                string accountNumber = input[1];
+                int balance = int.Parse(input[2]);
+                companies[i] = companyName;
+                bankAccountTable[companyName] =
+                    new Tuple<string, int>(accountNumber, balance);
+            }
+            // 取引入力
+            string[] requests = new string[k];
+            for (int i = 0; i < k; i++)
+            {
+                requests[i] = Console.ReadLine();
+            }
+
+            // requests の処理
+            foreach (var request in requests)
+            {
+                string[] requestParams = request.Split();
+                string companyName = requestParams[0];
+                string accountNumber = requestParams[1];
+                int withdrawalAmount = int.Parse(requestParams[2]);
+                string selectedAccountNumber = bankAccountTable[companyName].Item1;
+                int balance = bankAccountTable[companyName].Item2;
+
+                if (selectedAccountNumber == accountNumber)
+                {
+                    bankAccountTable[companyName] =
+                        new Tuple<string, int>(accountNumber,
+                                               balance - withdrawalAmount);
+                }
+            }
+
+            // 出力
+            foreach (var company in companies)
+            {
+                int balance = bankAccountTable[company].Item2;
+                Console.WriteLine($"{company} {balance}");
+            }
         }
     }
 }
@@ -19,9 +69,12 @@ namespace _02_03_bank
 https://paiza.jp/works/mondai/query_primer/query_primer__bank
 問題文のURLをコピーする
  下記の問題をプログラミングしてみよう！
-2xxx年に paiza が設立した paiza 中央銀行に勤務する pai沢直樹は、故障した ATM の対応として、お金を引き出したい会社と電話をして、会社名が銀行の名簿に登録されており、かつ、会社側が会社の口座の暗証番号を正しく言えた場合にのみ現金を支払い、それを記帳するという業務を任されていました。
+2xxx年に paiza が設立した paiza 中央銀行に勤務する pai沢直樹は、故障した ATM の対応として、
+お金を引き出したい会社と電話をして、会社名が銀行の名簿に登録されており、かつ、
+会社側が会社の口座の暗証番号を正しく言えた場合にのみ現金を支払い、それを記帳するという業務を任されていました。
 
-銀行に登録されている会社名とその口座の暗証番号と残高についての情報、また、直樹の電話の情報が与えられるので、全ての取引が終了した後の全ての会社の残高を出力してください。
+銀行に登録されている会社名とその口座の暗証番号と残高についての情報、また、直樹の電話の情報が与えられるので、
+全ての取引が終了した後の全ての会社の残高を出力してください。
 
 ▼　下記解答欄にコードを記入してみよう
 
@@ -36,8 +89,10 @@ G_K M_K W_K
 
 
 ・1 行目では、銀行に登録されている会社の数 N と行った取引の数 K が与えられます。
-・続く N 行のうち、i 行目では、i 番目に登録されている会社名 C_i とその口座の暗証番号 P_i と残高 D_i が与えられます。
-・続く K 行のうち、i 行目では、i 回目の取引を行おうとした会社の名前 G_i と、その人が言った暗証番号 M_i , 引出そうとした金額 W_i が与えられます。
+・続く N 行のうち、i 行目では、i 番目に登録されている会社名 C_i とその口座の暗証番号 P_i と残高 D_i が
+与えられます。
+・続く K 行のうち、i 行目では、i 回目の取引を行おうとした会社の名前 G_i と、その人が言った暗証番号 M_i ,
+引出そうとした金額 W_i が与えられます。
 
 入力値最終行の末尾に改行が１つ入ります。
 文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
