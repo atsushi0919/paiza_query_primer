57cf3c40d14042d8de582e76f671f6775053b2c3
atsushi0919 <hatake0919@yahoo.co.jp>
2022-01-19 09:52:39 +0900

    03-02_interval_sum C# AC

--- 03-02_interval_sum/Program.cs
+++ 03-02_interval_sum/Program.cs
@@ -4,89 +4,36 @@ namespace _03_02_interval_sum
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // 入力
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int k = int.Parse(input[1]);
+            int[] array = new int[n];
+            int[] sectionSum = new int[n + 1];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                array[i] = value;
+                sum += value;
+                sectionSum[i + 1] = sum;
+            }
+            int[][] sections = new int[k][];
+            for (int i = 0; i < k; i++)
+            {
+                sections[i] = Array.ConvertAll(
+                    Console.ReadLine().Split(), int.Parse);
+            }
+
+            // 出力
+            foreach (var section in sections)
+            {
+                int l = section[0];
+                int r = section[1];
+                Console.WriteLine(sectionSum[r] - sectionSum[l - 1]);
+            }
         }
     }
 }
-
-/*
-区間和 (paizaランク C 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
-
-シェア用URL:
-https://paiza.jp/works/mondai/query_primer/query_primer__interval_sum
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-長さ N の数列 A と、K 個の区間 (l_1,r_1) ... (l_K,r_K) が与えられるので、各区間についての A の区間和 A_{l_i} + ... + A_{r_i} (1 ≦ i ≦ K) を求めてください。
-
-▼　下記解答欄にコードを記入してみよう
-
-入力される値
-N K
-A_1
-...
-A_N
-l_1 r_1
-...
-l_K r_K
-
-
-・1 行目では、配列 A の要素数 N と与えられる整数の数 K が与えられます。
-・続く N 行では、配列 A の要素が A_1 から順に与えられます。
-・続く K 行では、和を求めるのに使う区間の値 l , r が与えられます。
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-ans_1
-...
-ans_K
-
-
-・i 行目に A_{l_i} + ... + A_{r_i} の和 ans_i (1 ≦ i ≦ K)を出力してください。
-・また、出力の末尾には改行を入れてください。
-条件
-・1 ≦ N , K ≦ 100,000
-・-100 ≦ A_i ≦ 100 (1 ≦ i ≦ N)
-・1 ≦ l_i ≦ r_i ≦ N (1 ≦ i ≦ K)
-
-入力例1
-4 2
-16
-88
-10
--65
-2 4
-1 2
-
-出力例1
-33
-104
-
-入力例2
-10 5
-82
--37
-40
--72
--24
--54
-57
--6
-42
--24
-8 9
-6 9
-2 3
-4 4
-1 5
-
-出力例2
-36
-39
-3
--72
--11
-*/
